f6e4119e081c01c0d82fed0a9260a26ea48729f2
EstelleMyddleware <estellegaits@myddleware.com>
2022-02-25 12:24:26 +0100

    Security : User, Registration & Verify email

---
+++ src/Controller/TrickController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Trick;
+use App\Form\TrickType;
+use App\Repository\TrickRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/trick')]
+class TrickController extends AbstractController
+{
+    #[Route('/', name: 'app_trick_index', methods: ['GET'])]
+    public function index(TrickRepository $trickRepository): Response
+    {
+        return $this->render('trick/index.html.twig', [
+            'tricks' => $trickRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_trick_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, TrickRepository $trickRepository): Response
+    {
+        $trick = new Trick();
+        $form = $this->createForm(TrickType::class, $trick);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $trickRepository->add($trick);
+            return $this->redirectToRoute('app_trick_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('trick/new.html.twig', [
+            'trick' => $trick,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_trick_show', methods: ['GET'])]
+    public function show(Trick $trick): Response
+    {
+        return $this->render('trick/show.html.twig', [
+            'trick' => $trick,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_trick_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Trick $trick, TrickRepository $trickRepository): Response
+    {
+        $form = $this->createForm(TrickType::class, $trick);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $trickRepository->add($trick);
+            return $this->redirectToRoute('app_trick_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('trick/edit.html.twig', [
+            'trick' => $trick,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_trick_delete', methods: ['POST'])]
+    public function delete(Request $request, Trick $trick, TrickRepository $trickRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$trick->getId(), $request->request->get('_token'))) {
+            $trickRepository->remove($trick);
+        }
+
+        return $this->redirectToRoute('app_trick_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
