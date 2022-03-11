fc6ad495cee41abee98a2960d64726baa4d8efd6
EstelleMyddleware <estellegaits@myddleware.com>
2022-03-11 09:32:42 +0100

    feat: use username to log in to app instead of email

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -12,18 +12,13 @@ class LoginController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        // try {
-
-        // } catch (Exception $e){
-        //     $this->logger->info('Email verif');
-        // }
-        // get the error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-        return $this->render('login/index.html.twig', [
-            'last_username' => $lastUsername,
-            'error'         => $error,
-        ]);
+            // get the error if there is one
+            $error = $authenticationUtils->getLastAuthenticationError();
+            // last username entered by the user
+            $lastUsername = $authenticationUtils->getLastUsername();
+            return $this->render('login/index.html.twig', [
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            ]);
     }
 }
