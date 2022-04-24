9efcf825b0dc561d1c3b9902660b6495e7d1614d
EstelleMyddleware <estellegaits@myddleware.com>
2022-04-24 23:08:02 +0200

    add trcik, category & user fixtures

--- src/Form/TrickType.php
+++ src/Form/TrickType.php
@@ -3,14 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Trick;
-use App\Form\ImageType;
-use App\Form\VideoType;
-use App\Form\CategoryType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class TrickType extends AbstractType
 {
