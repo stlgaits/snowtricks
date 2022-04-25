f23ed05e48b22f694cbb7d6ec30c260e69a5a597
EstelleMyddleware <estellegaits@myddleware.com>
2022-04-25 23:39:30 +0200

    add ImageFixtures

---
+++ src/DataFixtures/ImageFixtures.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Image;
+use App\Repository\TrickRepository;
+use App\Service\FileUploader;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class ImageFixtures extends Fixture implements DependentFixtureInterface
+{
+    protected $trickRepository;
+    protected $fileUploader;
+
+    public function __construct(TrickRepository $trickRepository, FileUploader $fileUploader)
+    {
+        $this->trickRepository = $trickRepository;
+        $this->fileUploader = $fileUploader;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+        $tricks = $this->trickRepository->findAll();
+        $imageFiles = [];
+        for ($i = 0; $i < $faker->randomNumber(2); $i++) {
+            $imageFiles[] = $faker->image();
+        }
+        foreach ($imageFiles as $imageFile) {
+            $image = new Image();
+            $image->setFileName($this->fileUploader->upload($imageFile))
+                    ->setPath('/uploads/images'.$image->getFileName())
+                    ->setTrick($faker->randomElement($tricks))
+            ;
+            $manager->persist($image);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [TrickFixtures::class];
+    }
+}
