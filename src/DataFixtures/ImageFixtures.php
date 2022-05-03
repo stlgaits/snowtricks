80105e0b898d42bc72f5645480ef7e762e862a3e
EstelleMyddleware <estellegaits@myddleware.com>
2022-05-03 17:46:56 +0200

    feat: thumbnail image set to first image of the images list, else placeholder image

--- src/DataFixtures/ImageFixtures.php
+++ src/DataFixtures/ImageFixtures.php
@@ -2,13 +2,15 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Image;
-use App\Repository\TrickRepository;
-use App\Service\FileUploader;
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
-use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use App\Entity\Image;
+use App\Service\FileUploader;
+use App\Repository\TrickRepository;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
 class ImageFixtures extends Fixture implements DependentFixtureInterface
 {
@@ -25,17 +27,35 @@ class ImageFixtures extends Fixture implements DependentFixtureInterface
     {
         $faker = Factory::create('fr_FR');
         $tricks = $this->trickRepository->findAll();
-        $imageFiles = [];
-        for ($i = 0; $i < $faker->randomNumber(2); ++$i) {
-            $imageFiles[] = $faker->image('./public/uploads/images', 640, 480, fullPath:false);
-        }
-        foreach ($imageFiles as $imageFile) {
-            $image = new Image();
-            $image->setFileName($imageFile)
-                    ->setPath('/uploads/images/'.$image->getFileName())
-                    ->setTrick($faker->randomElement($tricks))
-            ;
-            $manager->persist($image);
+        $imagesFolder = realpath('./assets/images/image_fixtures/');
+        // $imagesFolder = pathinfo('./assets/images/image_fixtures/');
+        $imageFiles = scandir($imagesFolder);
+        $parentDirs[0] = array_shift($imageFiles);
+        $parentDirs[1] = array_shift($imageFiles);
+        // $imageFiles = array_shift($imageFiles);
+        // var_dump($imageFiles);
+        // die();
+        // for ($i = 0; $i < $faker->randomNumber(2); ++$i) {
+            // $imageFiles[] =  $this->fileUploader->upload($imageFile);
+            // $imageFiles[] = $faker->image('./public/uploads/images', 640, 480, fullPath:false);
+        // }
+        foreach ($imageFiles as $key =>  $imageFile) {
+            $filename = $key.'.jpg';
+            $imageFile= strval($imagesFolder."\\".$imageFile);
+            // $imageFile= pathinfo($imageFile)['filename'];
+            $file = new UploadedFile($imageFile, $filename);
+            if(is_file($file)){
+                $this->fileUploader->upload($file);
+                $image = new Image();
+                $image->setFileName($imageFile)
+                        ->setPath('/uploads/images/'.$image->getFileName())
+                        ->setTrick($faker->randomElement($tricks))
+                ;
+                $manager->persist($image);
+                echo PHP_EOL."OUI".PHP_EOL;
+            } else {
+                echo PHP_EOL."NON".PHP_EOL;
+            }
         }
 
         $manager->flush();
