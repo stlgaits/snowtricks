c7e1cd2ce95f07254fc58d4a46e1a69d6b533010
EstelleMyddleware <estellegaits@myddleware.com>
2022-05-19 13:39:30 +0200

    feat: add user profile page & add some background images on login form & visual improvements of comments card

---
+++ src/Controller/UserProfileController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_user_profile')]
+    public function myProfile(): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $this->getUser();
+        return $this->render('user_profile/index.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
