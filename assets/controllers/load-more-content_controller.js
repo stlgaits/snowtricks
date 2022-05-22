40e8cbd2e62059e18c709167e2c75371c9e4de41
EstelleMyddleware <estellegaits@myddleware.com>
2022-05-23 00:54:49 +0200

    feat load more tricks using AJAX (axios) request

--- assets/controllers/load-more-content_controller.js
+++ assets/controllers/load-more-content_controller.js
@@ -6,50 +6,50 @@ export default class extends Controller {
     static targets = ['trick', 'tricks'];
     static values = {
         max: Number,
-        infoUrl: String
+        infoUrl: String,
+        offset: 0,
+        page: 1
     }
 
-    connect() {
-        console.log(this.infoUrlValue)
-        // console.log(this.trickTargets);
-        // this.tricksTarget.innerHTML = "Loading...";
-        // console.log(this.maxValue);
+    connect()
+    {
         this.trickTargets.forEach((element) => {
-            console.log(element);
+            this.offsetValue++;
         });
-        // this.element.textContent = 'Hello Stimulus! Edit me in assets/controllers/hello_controller.js';
     }
 
     onLoadMore(event)
     {
-        console.log(event);
-        // GET request for remote image in node.js
-        // axios({
-        //     method: 'get',
-        //     url: 'http://bit.ly/2mTM3nY',
-        //     responseType: 'stream'
-        // })
-        //     .then(function (response) {
-        //     response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
-        //     });
+        this.loadAnimation(event);
         try {
-            axios.get(this.infoUrlValue)
-            .then((response) => {
-                console.log(response)
-                // const audio = new Audio(response.data.url);
-                // audio.play();
-            });
-        } catch (e) {
+            if(this.offsetValue >= this.maxValue) {
+                this.pageValue++;
+                axios.get(this.infoUrlValue, {  
+                    params: { 
+                        page: this.pageValue,
+                        offset : this.offsetValue
+                    } 
+                    })
+                .then((response) => {
+                    this.tricksTarget.innerHTML += response.data;
+                });
+            }
+        } catch(e) {
             console.log(e.responseText)
         }
-        // console.log(this.maxValue);
-        // console.log(this.tricksTarget);
-        // console.log(event.currentTarget);
-        // this.trickTargets.forEach((element) => {
-        //     console.log(element);
-        // });
+    }
 
-        // this.tricksTarget.appendChild(moreTricks);
+    async loadAnimation(event) {
+        try {
+            const buttonText = event.currentTarget.innerText;
+            const button = event.currentTarget;
+            button.innerText = '...';
+            await setTimeout(function(){
+                button.innerText = buttonText;
+            }, 500)
+        } catch(e) {
+            console.log(e.responseText)
+        }
     }
 
 }
